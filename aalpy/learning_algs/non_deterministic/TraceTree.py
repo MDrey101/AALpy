f9a2b1d907468ba84199779baa1b65af0d552e4c
Manuel Doler <mdoler@student.tugraz.at>
2022-10-25 14:32:46 +0200

    .

---
+++ aalpy/learning_algs/non_deterministic/TraceTree.py
@@ -0,0 +1,184 @@
+from collections import defaultdict, Counter
+
+from aalpy.base import SUL
+
+
+class SULWrapper(SUL):
+    """
+    Wrapper for non-deterministic SUL. After every step, input/output pair is added to the tree containing all traces.
+    """
+
+    def __init__(self, sul: SUL):
+        super().__init__()
+        self.sul = sul
+        self.pta = TraceTree()
+
+    def pre(self):
+        self.pta.reset()
+        self.sul.pre()
+
+    def post(self):
+        self.sul.post()
+
+    def step(self, letter):
+        out = self.sul.step(letter)
+        self.pta.add_to_tree(letter, out)
+        return out
+
+
+class Node:
+    __slots__ = ['output', 'children', 'parent', 'frequency_counter']
+
+    def __init__(self, output):
+        self.output = output
+        self.children = defaultdict(list)
+        self.parent = None
+
+        # frq counter
+        self.frequency_counter = Counter()
+
+    def get_child(self, inp, out):
+        """
+        Args:
+          inp:
+          out:
+
+        Returns:
+
+        """
+        return next((child for child in self.children[inp] if child.output == out), None)
+
+    def get_prefix(self):
+        prefix = ()
+        curr_node = self
+        while curr_node.parent is not None:
+            prefix = (curr_node.output,) + prefix
+            curr_node = curr_node.parent
+        return prefix
+
+
+class TraceTree:
+    """
+    Tree used for keeping track of seen observations.
+    """
+
+    def __init__(self):
+        self.root_node = Node(None)
+        self.curr_node = None
+
+    def reset(self):
+        self.curr_node = self.root_node
+
+    def add_to_tree(self, inp, out):
+        """
+        Adds new element to tree and makes it the current node
+
+        Args:
+
+          inp: Input
+          out: Output
+
+        """
+        if inp not in self.curr_node.children.keys() or \
+                out not in {child.output for child in self.curr_node.children[inp]}:
+            node = Node(out)
+            self.curr_node.children[inp].append(node)
+            node.parent = self.curr_node
+
+        self.curr_node.frequency_counter[(inp,out)] += 1
+        self.curr_node = self.curr_node.get_child(inp, out)
+
+    def get_to_node(self, inputs, outputs):
+        """
+        Follows the path described by inp and out and returns the node which is reached
+
+        Args:
+          inputs: Inputs
+          outputs: Outputs
+
+        Returns:
+
+          Node that is reached when following the given input and output through the tree
+        """
+        curr_node = self.root_node
+        for i, o in zip(inputs, outputs):
+            node = curr_node.get_child(i, o)
+            if node is None:
+                return None
+            curr_node = node
+
+        return curr_node
+
+    def get_all_traces(self, curr_node=None, e=None):
+        """
+
+        Args:
+
+          curr_node: current node
+          e: List of inputs
+
+        Returns:
+
+          Traces of outputs corresponding to the input-sequence given by e
+        """
+
+        if not curr_node or not e:
+            return []
+
+        queue = [(curr_node, 0)]
+        reached_nodes = []
+        while queue:
+            node, depth = queue.pop(0)
+            if depth == len(e):
+                reached_nodes.append(node)
+            else:
+                children_with_same_input = node.children[e[depth]]
+                for c in children_with_same_input:
+                    queue.append((c, depth + 1))
+
+        cell = [node.get_prefix()[-len(e):] for node in reached_nodes]
+        return cell
+
+    def prune(self, threshold=3):
+
+        queue = [self.root_node]
+        while queue:
+            curr_node = queue.pop(0)
+            to_delete = []
+            for inp in curr_node.children.keys():
+                children = curr_node.children[inp]
+                for child in children:
+                    child_out = child.output
+                    #if curr_node.frequency_counter[(inp, child_out)] >= threshold:
+                    if "DANGER" != child_out:
+                        queue.append(child)
+                    else:
+                        to_delete.append((inp, child_out))
+
+            for i,o in to_delete:
+                print("Pruning")
+                delete_candidate = curr_node.get_child(i, o)
+                curr_node.children[i].remove(delete_candidate)
+                curr_node.frequency_counter[(i, o)] = 0
+
+
+    def get_table(self, s, e):
+        """
+        Generates a table from the tree
+
+        Args:
+          s: rows from S, S_dot_A, or both which should be presented in the table.
+          e: E
+
+        Returns:
+          a table in a format that can be used for printing.
+        """
+        result = {}
+        for pair in s:
+            curr_node = self.get_to_node(pair[0], pair[1])
+            result[pair] = {}
+
+            for inp in e:
+                result[pair][inp] = self.get_all_traces(curr_node, inp)
+
+        return result
---
+++ tester.py
@@ -0,0 +1,44 @@
+from random import choice, choices
+
+from aalpy.automata import Onfsm
+from aalpy.base import SUL
+from aalpy.learning_algs import run_non_det_Lstar
+from aalpy.oracles import RandomWordEqOracle
+from aalpy.SULs import OnfsmSUL
+from aalpy.utils import load_automaton_from_file
+
+model = load_automaton_from_file("fail_safe_model.dot", "onfsm")
+alphabet = model.get_input_alphabet()
+
+
+class FailSUL(SUL):
+    def __init__(self, mdp: Onfsm):
+        super().__init__()
+        self.onfsm = mdp
+
+    def pre(self):
+        self.onfsm.reset_to_initial()
+
+    def post(self):
+        pass
+
+    def step(self, letter):
+        if "DANGER" in self.onfsm.outputs_on_input(letter):
+            possible_states = self.onfsm.current_state.transitions[letter]
+            danger_position = ["DANGER" == state[0] for state in possible_states]
+            probability_distributions = [0.1 if d else 0.9 for d in danger_position]
+            transition = choices(possible_states, probability_distributions, k=1)[0]
+        else:
+            transition = choice(self.onfsm.current_state.transitions[letter])
+        output = transition[0]
+        self.onfsm.current_state = transition[1]
+        return output
+
+
+sul = FailSUL(model)
+
+eq_oracle = RandomWordEqOracle(alphabet, sul, num_walks=1000, min_walk_len=4, max_walk_len=8)
+
+learned_model = run_non_det_Lstar(alphabet, sul, eq_oracle, n_sampling=10,)
+
+learned_model.visualize()
